b15c8f40e4c6e4e25ae037908eaf12b11e0c8b5f
Samarth Paboowal <samarthpaboowal@Samarths-MacBook-Air.local>
2016-08-25 14:55:15 +0530

    Created UI for the application

--- Poke Finder/ViewController.swift
+++ Poke Finder/ViewController.swift
@@ -7,19 +7,19 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func generateRandomPokemon(sender: UIButton) {
+        
     }
-
-
 }
 
