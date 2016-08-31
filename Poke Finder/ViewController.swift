8d78c4eb6e2ba62f1af934d484efb47b972ad9ca
Samarth Paboowal <samarthpaboowal@Samarths-MacBook-Air.local>
2016-08-31 20:03:53 +0530

    Fixed some bugs to detect correct user location using apple maps

--- Poke Finder/ViewController.swift
+++ Poke Finder/ViewController.swift
@@ -9,13 +9,73 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    var mapHasCentredOnce = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        mapView.delegate = self
+        locationManager.delegate = self
+        mapView.userTrackingMode = MKUserTrackingMode.Follow
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        
+        locationAuthStatus()
+    }
+    
+    func locationAuthStatus() {
+        
+        if CLLocationManager.authorizationStatus() == .AuthorizedWhenInUse {
+            
+            mapView.showsUserLocation = true
+        } else {
+            
+            locationManager.requestWhenInUseAuthorization()
+            mapView.showsUserLocation = true
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        
+        if status == .AuthorizedWhenInUse {
+            
+            mapView.showsUserLocation = true
+        }
+    }
+    
+    func centreMapLocation(location: CLLocation) {
+        
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, 2000, 2000)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    func mapView(mapView: MKMapView, didUpdateUserLocation userLocation: MKUserLocation) {
+        
+        if let location = userLocation.location {
+            
+            if !mapHasCentredOnce {
+                centreMapLocation(location)
+                mapHasCentredOnce = true
+            }
+        }
+    }
+    
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        var annotationView: MKAnnotationView?
+        
+        if annotation.isKindOfClass(MKUserLocation.self) {
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "User")
+            annotationView?.image = UIImage(named: "ash")
+        }
+        
+        return annotationView
     }
 
     @IBAction func generateRandomPokemon(sender: UIButton) {
